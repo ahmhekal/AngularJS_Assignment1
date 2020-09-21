6f2fe2afc350f185e4ded12f9809e6456ab2a5e8
ahmhekal <ahmhekal@gmail.com>
2020-09-21 21:27:03 +0200

    enhancements and adding bonus

--- app.js
+++ app.js
@@ -8,21 +8,29 @@ MsgFnction.$inject = ['$scope'];
 function MsgFnction($scope) {
   $scope.str ="";
   $scope.msg = ''
+  $scope.color = 'green';
   $scope.DisplayMsg = function()
   {
     $scope.msg = CheckIfTooMuch($scope.str);
-  }
+    if ($scope.msg == "Please enter data first"){$scope.color = 'red'}
+    else {$scope.color = 'green'};
+  };
 
 
 }
 
 function CheckIfTooMuch (str) {
   var items = str.split(',');
-  var itemsCount=items.length;
+  var itemsFiltered = items.filter(function (i) {
+  return i != "";
+  });
+  var itemsCount=itemsFiltered.length;
   if (itemsCount >3)
   {
   return "Too much!"}
-  else {
+  else if (str=="") {
+    return "Please enter data first"}
+  else{
   return "Enjoy!"
   }
 
